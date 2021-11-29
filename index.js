0f43d08e856029dad628e54b4d10b5498f6c6d0f
Sam Lynch <samdawkins1998@gmail.com>
2021-11-29 17:50:46 -0500

    commit tree 54898418e84ef853c684ccfa326e0a0c45053406

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 // Your code goes here
+ document.addEventListener("DOMContentLoaded", () => {
+     return text.textContent = 'This is really cool!'
+     })
+
+// function handleText(){
+//     // let p = document.querySelector('p')
+//     let text = document.querySelector('text')
+//     text.textContent = 'This is really cool!'
+//     document.querySelector('text').appendChild(text)
+// }
+
+
+
+//  text.textContent = 'This is really cool!'
